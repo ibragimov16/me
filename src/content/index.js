045ed6393cd3236f0ca40c1b2d23b47f1aaf5a69
Artur <parkour20_99@mail.ru>
2019-03-14 12:03:40 +0300

    s

--- src/content/index.js
+++ src/content/index.js
@@ -3,8 +3,12 @@ import React, {Component} from 'react'
 export default class Content extends Component{
     render(){
         return(
-            <div className="content container">
-            
+            <div className="content">
+                <div className="content-left-block">
+                    <ul>
+                        <li>dadada</li>
+                    </ul>
+                </div>
             </div>
         )
     }
